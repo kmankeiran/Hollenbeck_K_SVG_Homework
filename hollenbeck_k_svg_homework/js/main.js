cce949229e3f0386c690cb021874588a7ad60b97
kmankeiran <43183043+kmankeiran@users.noreply.github.com>
2019-01-25 17:00:30 -0500

    Add files via upload

---
+++ hollenbeck_k_svg_homework/js/main.js
@@ -0,0 +1,16 @@
+(() => { 
+var otherIcons = document.querySelectorAll('.icon');
+
+
+	function iconJeans(jeans) {
+		console.log(this.Jeans);
+		debugger;
+	}
+
+
+icon.addEventListener("click", Jeans);
+
+
+otherIcons.forEach(thisicon => thisicon.addEventListener("click", Jeans));	
+
+})();
